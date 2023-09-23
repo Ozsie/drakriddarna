a7ac312c752f4cea5efe320e5a38f962b5c0a895
Oscar Djupfeldt <oscar.djupfeldt@abilia.se>
2023-09-23 19:12:01 +0200

    Lint fixes on monster logic and rendering

--- src/monsters/MonsterLogic.ts
+++ src/monsters/MonsterLogic.ts
@@ -1,9 +1,4 @@
-import type {
-  GameState,
-  Hero,
-  Monster,
-  Position
-} from "../types";
+import type { GameState, Hero, Monster, Position } from '../types';
 import {
   addLog,
   findCell,
@@ -13,14 +8,14 @@ import {
   isDiscovered,
   isWalkable,
   stepAlongLine,
-  takeDamage
-} from "../game";
-import { liveHeroes } from "../hero/HeroLogic";
+  takeDamage,
+} from '../game';
+import { liveHeroes } from '../hero/HeroLogic';
 
 enum MonsterAction {
   RANGED_ATTACK = 'RANGED_ATTACK',
   MELEE_ATTACK = 'MELEE_ATTACK',
-  MOVE = 'MOVE'
+  MOVE = 'MOVE',
 }
 
 export const monsterActions = (state: GameState) => {
@@ -30,13 +25,20 @@ export const monsterActions = (state: GameState) => {
   }
 
   visibleMonsters.forEach((monster) => {
-    const maxActions = monster.actions
+    const maxActions = monster.actions;
     addLog(state, `${monster.name} acted `);
     while (monster.actions > 0) {
-      const neighbouringHeroes: Hero[] = findNeighbouringHeroes(state, monster)
-        .filter((hero: Hero) => !hero.ignoredByMonsters);
+      const neighbouringHeroes: Hero[] = findNeighbouringHeroes(
+        state,
+        monster,
+      ).filter((hero: Hero) => !hero.ignoredByMonsters);
       const visibleHeroes: Hero[] = findVisibleHeroes(state, monster);
-      const action = selectAction(state, monster, neighbouringHeroes, visibleHeroes);
+      const action = selectAction(
+        state,
+        monster,
+        neighbouringHeroes,
+        visibleHeroes,
+      );
       switch (action) {
         case MonsterAction.MELEE_ATTACK: {
           const target: Hero = selectMeleeTarget(neighbouringHeroes);
@@ -58,9 +60,14 @@ export const monsterActions = (state: GameState) => {
     monster.actions = maxActions;
     monster.movement = getEffectiveMaxMovement(monster);
   });
-}
+};
 
-const selectAction = (state: GameState, monster: Monster, neighbouringHeroes: Hero[], visibleHeroes: Hero[]): MonsterAction => {
+const selectAction = (
+  state: GameState,
+  monster: Monster,
+  neighbouringHeroes: Hero[],
+  visibleHeroes: Hero[],
+): MonsterAction => {
   if (neighbouringHeroes.length > 0) {
     return MonsterAction.MELEE_ATTACK;
   } else if (!monster.rangedWeapon) {
@@ -68,7 +75,10 @@ const selectAction = (state: GameState, monster: Monster, neighbouringHeroes: He
   } else {
     if (visibleHeroes.length > 0 && monster.rangedWeapon) {
       if (Math.random() < 0.1) {
-        addLog(state, `${monster.name} decided to move despite seeing a target.`);
+        addLog(
+          state,
+          `${monster.name} decided to move despite seeing a target.`,
+        );
         return MonsterAction.MOVE;
       }
       return MonsterAction.RANGED_ATTACK;
@@ -76,59 +86,80 @@ const selectAction = (state: GameState, monster: Monster, neighbouringHeroes: He
       return MonsterAction.MOVE;
     }
   }
-}
+};
 
-const selectRangedTarget = (possibleTargets: Hero[], monster: Monster): Hero => {
-  return possibleTargets
+const selectRangedTarget = (possibleTargets: Hero[], monster: Monster): Hero =>
+  possibleTargets
     .filter((hero) => !hero.shield)
     .sort((hero) => getDist(monster.position, hero.position))
-    .sort((a, b) => {
-      return b.health - a.health || getDist(monster.position, a.position) - getDist(monster.position, b.position);
-    })[0];
-}
+    .sort(
+      (a, b) =>
+        b.health - a.health ||
+        getDist(monster.position, a.position) -
+          getDist(monster.position, b.position),
+    )[0];
 
-const selectMeleeTarget = (possibleTargets: Hero[]): Hero => {
-  return possibleTargets.sort((a, b) => {
-    return b.health - a.health;
-  })[0];
-}
+const selectMeleeTarget = (possibleTargets: Hero[]): Hero =>
+  possibleTargets.sort((a, b) => b.health - a.health)[0];
 
-const findVisibleHeroes = (state: GameState, monster: Monster): Hero[] => {
-  return liveHeroes(state)
+const findVisibleHeroes = (state: GameState, monster: Monster): Hero[] =>
+  liveHeroes(state)
     .filter((hero: Hero) => !hero.ignoredByMonsters)
     .filter((hero) => {
-      const startPixelPos = { x: (monster.position.x * 48) - 24, y: (monster.position.y * 48) - 24 };
-      const targetPixelPos = { x: (hero.position.x * 48) - 24, y: (hero.position.y * 48) - 24 };
-      return stepAlongLine(startPixelPos, monster.position, targetPixelPos, hero.position, state, []);
+      const startPixelPos = {
+        x: monster.position.x * 48 - 24,
+        y: monster.position.y * 48 - 24,
+      };
+      const targetPixelPos = {
+        x: hero.position.x * 48 - 24,
+        y: hero.position.y * 48 - 24,
+      };
+      return stepAlongLine(
+        startPixelPos,
+        monster.position,
+        targetPixelPos,
+        hero.position,
+        state,
+        [],
+      );
     });
-}
 
-const findVisibleMonsters = (state: GameState) => {
-  return state.dungeon.layout.monsters.filter((monster) => {
-    const cell = findCell(state.dungeon.layout.grid, monster.position.x, monster.position.y);
-    return cell && state.dungeon.discoveredRooms.includes(cell) && monster.health > 0;
+const findVisibleMonsters = (state: GameState) =>
+  state.dungeon.layout.monsters.filter((monster) => {
+    const cell = findCell(
+      state.dungeon.layout.grid,
+      monster.position.x,
+      monster.position.y,
+    );
+    return (
+      cell && state.dungeon.discoveredRooms.includes(cell) && monster.health > 0
+    );
   });
-}
 
-const monsterAttack = (state: GameState, monster: Monster, hero: Hero, ranged: boolean) => {
+const monsterAttack = (
+  state: GameState,
+  monster: Monster,
+  hero: Hero,
+  ranged: boolean,
+) => {
   takeDamage(state, monster, hero, ranged);
   if (monster?.actions > 1 && monster?.movement < 3) {
-    monster.actions -= 2
+    monster.actions -= 2;
   } else {
     monster.actions--;
   }
-}
+};
 
-const findClosestHeroAndDistance = (state: GameState, monster: Monster): { hero: Hero, dist: number } => {
-  return liveHeroes(state).map((hero) => {
-    return {
+const findClosestHeroAndDistance = (
+  state: GameState,
+  monster: Monster,
+): { hero: Hero; dist: number } =>
+  liveHeroes(state)
+    .map((hero) => ({
       hero,
-      dist: getDist(hero.position, monster.position)
-    };
-  }).sort((a, b) => {
-    return a.dist - b.dist;
-  })[0];
-}
+      dist: getDist(hero.position, monster.position),
+    }))
+    .sort((a, b) => a.dist - b.dist)[0];
 
 const monsterMove = (state: GameState, monster: Monster) => {
   const closestHeroAndDistance = findClosestHeroAndDistance(state, monster);
@@ -137,16 +168,17 @@ const monsterMove = (state: GameState, monster: Monster) => {
     if (closestHeroAndDistance.dist > 1) {
       const possibleMoves = findPossibleMoves(state, monster.position);
       if (possibleMoves.length > 0) {
-        const newPosition = possibleMoves.map((pos) => {
-          return {
+        const newPosition = possibleMoves
+          .map((pos) => ({
             pos,
-            dist: getDist(pos, closestHeroAndDistance.hero.position)
-          };
-        }).sort((a, b) => {
-          return a.dist - b.dist;
-        })[0].pos;
+            dist: getDist(pos, closestHeroAndDistance.hero.position),
+          }))
+          .sort((a, b) => a.dist - b.dist)[0].pos;
         monster.position = newPosition;
-        addLog(state, `${monster.name} moved towards ${closestHeroAndDistance.hero.name} (${newPosition.x},${newPosition.y})`);
+        addLog(
+          state,
+          `${monster.name} moved towards ${closestHeroAndDistance.hero.name} (${newPosition.x},${newPosition.y})`,
+        );
       } else {
         addLog(state, `${monster.name} could not move`);
       }
@@ -154,25 +186,39 @@ const monsterMove = (state: GameState, monster: Monster) => {
     monster.movement--;
   }
   monster.actions--;
-}
+};
 
-const findPossibleMoves = (state: GameState, position: Position): Position[] => {
+const findPossibleMoves = (
+  state: GameState,
+  position: Position,
+): Position[] => {
   const x = position.x;
   const y = position.y;
 
   const targets: Position[] = [];
-  for (let tX = x-1; tX <= x+1; tX++) {
-    for (let tY = y-1; tY <= y+1; tY++) {
+  for (let tX = x - 1; tX <= x + 1; tX++) {
+    for (let tY = y - 1; tY <= y + 1; tY++) {
       if (tX === x && tY === y) continue;
-      if (isWalkable(state.dungeon.layout, tX, tY) && isDiscovered(state.dungeon, tX, tY)) {
-        targets.push({x:tX,y:tY});
+      if (
+        isWalkable(state.dungeon.layout, tX, tY) &&
+        isDiscovered(state.dungeon, tX, tY)
+      ) {
+        targets.push({ x: tX, y: tY });
       }
     }
   }
 
-  return targets.filter((pos) => {
-    return !liveHeroes(state).find((hero) => hero.position.x === pos.x && hero.position.y === pos.y);
-  }).filter((pos) => {
-    return !state.dungeon.layout.monsters.some((m) => m.position.x === pos.x && m.position.y === pos.y);
-  })
-}
+  return targets
+    .filter(
+      (pos) =>
+        !liveHeroes(state).find(
+          (hero) => hero.position.x === pos.x && hero.position.y === pos.y,
+        ),
+    )
+    .filter(
+      (pos) =>
+        !state.dungeon.layout.monsters.some(
+          (m) => m.position.x === pos.x && m.position.y === pos.y,
+        ),
+    );
+};
--- src/monsters/MonsterRendering.ts
+++ src/monsters/MonsterRendering.ts
@@ -1,74 +1,182 @@
-import type {
-  Hero,
-  GameState,
-  Monster,
-  Position
-} from "../types";
-import { MonsterType } from "../types";
-import { stepAlongLine } from "../game";
+import type { Hero, GameState, Monster, Position } from '../types';
+import { MonsterType } from '../types';
+import { stepAlongLine } from '../game';
 
-export const renderMonsters = (ctx: CanvasRenderingContext2D, actors: CanvasImageSource, cellSize: number, state: GameState, debugMode: boolean) => {
+export const renderMonsters = (
+  ctx: CanvasRenderingContext2D,
+  actors: CanvasImageSource,
+  cellSize: number,
+  state: GameState,
+  debugMode: boolean,
+) => {
   state.dungeon.layout.monsters.forEach((monster) => {
     renderMonster(ctx, actors, state, monster, cellSize, debugMode);
   });
-}
+};
 
-const renderMonster = (ctx: CanvasRenderingContext2D, actors: CanvasImageSource, state: GameState, monster: Monster, cellSize: number, debugMode: boolean) => {
-  const cell = state.dungeon.layout.grid[monster.position.y][monster.position.x]
+const renderMonster = (
+  ctx: CanvasRenderingContext2D,
+  actors: CanvasImageSource,
+  state: GameState,
+  monster: Monster,
+  cellSize: number,
+  debugMode: boolean,
+) => {
+  const cell =
+    state.dungeon.layout.grid[monster.position.y][monster.position.x];
   if (monster && monster.health > 0 && isInDiscoveredRoom(cell, state)) {
     switch (monster.type) {
-      case MonsterType.ORCH: renderOrch(ctx, actors, cellSize, monster); break;
-      case MonsterType.TROLL: renderTroll(ctx, actors, cellSize, monster); break;
-      default: renderDefaultMonster(ctx, actors, cellSize, monster); break;
+      case MonsterType.ORCH:
+        renderOrch(ctx, actors, cellSize, monster);
+        break;
+      case MonsterType.TROLL:
+        renderTroll(ctx, actors, cellSize, monster);
+        break;
+      default:
+        renderDefaultMonster(ctx, actors, cellSize, monster);
+        break;
     }
     renderActorBar(ctx, monster, cellSize);
     renderHealthBar(ctx, monster, cellSize);
     renderLineOfSight(ctx, monster, state.heroes, state, cellSize, debugMode);
   }
-}
+};
 
-const renderOrch = (ctx: CanvasRenderingContext2D, actors: CanvasImageSource, cellSize: number, monster: Monster) => {
-  ctx.drawImage(actors, cellSize, 16, 16, 16, monster.position.x * cellSize, monster.position.y * cellSize, cellSize, cellSize);
-}
+const renderOrch = (
+  ctx: CanvasRenderingContext2D,
+  actors: CanvasImageSource,
+  cellSize: number,
+  monster: Monster,
+) => {
+  ctx.drawImage(
+    actors,
+    cellSize,
+    16,
+    16,
+    16,
+    monster.position.x * cellSize,
+    monster.position.y * cellSize,
+    cellSize,
+    cellSize,
+  );
+};
 
-const renderTroll = (ctx: CanvasRenderingContext2D, actors: CanvasImageSource, cellSize: number, monster: Monster) => {
-  ctx.drawImage(actors, cellSize, 16, 16, 16, monster.position.x * cellSize, monster.position.y * cellSize, cellSize, cellSize);
-}
+const renderTroll = (
+  ctx: CanvasRenderingContext2D,
+  actors: CanvasImageSource,
+  cellSize: number,
+  monster: Monster,
+) => {
+  ctx.drawImage(
+    actors,
+    cellSize,
+    16,
+    16,
+    16,
+    monster.position.x * cellSize,
+    monster.position.y * cellSize,
+    cellSize,
+    cellSize,
+  );
+};
 
-const renderDefaultMonster = (ctx: CanvasRenderingContext2D, actors: CanvasImageSource, cellSize: number, monster: Monster) => {
-  ctx.drawImage(actors, 16, 16, 16, 16, monster.position.x * cellSize, monster.position.y * cellSize, cellSize, cellSize);
-}
+const renderDefaultMonster = (
+  ctx: CanvasRenderingContext2D,
+  actors: CanvasImageSource,
+  cellSize: number,
+  monster: Monster,
+) => {
+  ctx.drawImage(
+    actors,
+    16,
+    16,
+    16,
+    16,
+    monster.position.x * cellSize,
+    monster.position.y * cellSize,
+    cellSize,
+    cellSize,
+  );
+};
 
-const renderActorBar = (ctx: CanvasRenderingContext2D, monster: Monster, cellSize: number) => {
+const renderActorBar = (
+  ctx: CanvasRenderingContext2D,
+  monster: Monster,
+  cellSize: number,
+) => {
   ctx.beginPath();
   ctx.strokeStyle = monster.colour;
   ctx.fillStyle = monster.colour;
-  ctx.fillRect((monster.position.x * cellSize) + 4, (monster.position.y * cellSize) + (cellSize - 6), cellSize - 8, 4);
+  ctx.fillRect(
+    monster.position.x * cellSize + 4,
+    monster.position.y * cellSize + (cellSize - 6),
+    cellSize - 8,
+    4,
+  );
   ctx.stroke();
   ctx.strokeStyle = 'black';
   ctx.fillStyle = 'black';
-}
+};
 
-const renderHealthBar = (ctx: CanvasRenderingContext2D, monster: Monster, cellSize: number) => {
+const renderHealthBar = (
+  ctx: CanvasRenderingContext2D,
+  monster: Monster,
+  cellSize: number,
+) => {
   ctx.beginPath();
   ctx.strokeStyle = 'black';
   ctx.fillStyle = 'red';
-  ctx.fillRect((monster.position.x * cellSize) + 4, monster.position.y*cellSize, (cellSize - 8), 4);
+  ctx.fillRect(
+    monster.position.x * cellSize + 4,
+    monster.position.y * cellSize,
+    cellSize - 8,
+    4,
+  );
   ctx.fillStyle = 'green';
-  ctx.fillRect((monster.position.x * cellSize) + 4, monster.position.y*cellSize, (cellSize - 8) * (monster.health/monster.maxHealth), 4);
-  ctx.rect((monster.position.x * cellSize) + 4, monster.position.y*cellSize, (cellSize - 8), 4);
+  ctx.fillRect(
+    monster.position.x * cellSize + 4,
+    monster.position.y * cellSize,
+    (cellSize - 8) * (monster.health / monster.maxHealth),
+    4,
+  );
+  ctx.rect(
+    monster.position.x * cellSize + 4,
+    monster.position.y * cellSize,
+    cellSize - 8,
+    4,
+  );
   ctx.stroke();
   ctx.strokeStyle = 'black';
   ctx.fillStyle = 'black';
-}
+};
 
-const renderLineOfSight = (ctx: CanvasRenderingContext2D, from: Monster, to: Hero[], state: GameState, cellSize: number, debugMode: boolean) => {
+const renderLineOfSight = (
+  ctx: CanvasRenderingContext2D,
+  from: Monster,
+  to: Hero[],
+  state: GameState,
+  cellSize: number,
+  debugMode: boolean,
+) => {
   if (debugMode) {
     to.forEach((target) => {
       const seenCells: Position[] = [];
-      const startPixelPos = { x: (from.position.x * 48) - 24, y: (from.position.y * 48) - 24 };
-      const targetPixelPos = { x: (target.position.x * 48) - 24, y: (target.position.y * 48) - 24 };
-      const seen = stepAlongLine(startPixelPos, from.position, targetPixelPos, target.position, state, seenCells);
+      const startPixelPos = {
+        x: from.position.x * 48 - 24,
+        y: from.position.y * 48 - 24,
+      };
+      const targetPixelPos = {
+        x: target.position.x * 48 - 24,
+        y: target.position.y * 48 - 24,
+      };
+      const seen = stepAlongLine(
+        startPixelPos,
+        from.position,
+        targetPixelPos,
+        target.position,
+        state,
+        seenCells,
+      );
       if (seen) {
         const sX = from.position.x;
         const sY = from.position.y;
@@ -78,13 +186,14 @@ const renderLineOfSight = (ctx: CanvasRenderingContext2D, from: Monster, to: Her
         ctx.beginPath();
         ctx.strokeStyle = from.colour;
         ctx.lineWidth = 2;
-        ctx.moveTo(sX * cellSize + (cellSize / 2), sY * cellSize + (cellSize / 2));
-        ctx.lineTo(eX * cellSize + (cellSize / 2), eY * cellSize + (cellSize / 2));
+        ctx.moveTo(sX * cellSize + cellSize / 2, sY * cellSize + cellSize / 2);
+        ctx.lineTo(eX * cellSize + cellSize / 2, eY * cellSize + cellSize / 2);
         ctx.stroke();
         ctx.lineWidth = 1;
       }
     });
   }
-}
+};
 
-const isInDiscoveredRoom = (cell: string, state: GameState) => state.dungeon.discoveredRooms.includes(cell);
+const isInDiscoveredRoom = (cell: string, state: GameState) =>
+  state.dungeon.discoveredRooms.includes(cell);
