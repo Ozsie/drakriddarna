02811ec96df54b8948c4d72ecc8facf7121984f9
Oscar Djupfeldt <oscar.djupfeldt@abilia.se>
2023-10-02 21:26:54 +0200

    Default to swedish

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,7 +3,7 @@ import { loadTranslations } from '$lib/translations';
 export const prerender = true;
 
 export const load = async () => {
-  const initialLocale = 'en'; // get from cookie / url / fetch from server...
+  const initialLocale = 'sv'; // get from cookie / url / fetch from server...
 
   await loadTranslations(initialLocale); // keep this just before the `return`
 
