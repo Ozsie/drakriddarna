ebcb22bdea36e8d6137be664a4d1e4d0d3addb00
Oscar Djupfeldt <oscar.djupfeldt@abilia.se>
2026-09-24 19:13:50 +0200

    Add random item to testing grounds

--- src/campaigns/dungeons/testingGrounds.ts
+++ src/campaigns/dungeons/testingGrounds.ts
@@ -112,6 +112,7 @@ export const testingGrounds: Dungeon = defineDungeon({
       [7, 10, magicItems[3]],
       [8, 10, magicItems[4]],
       [8, 7, magicItems[5]],
+      [10, 10],
     ],
   },
   events: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
